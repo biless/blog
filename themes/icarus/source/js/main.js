291eaabd5577ee412038fb4c944a823c952f01cd
bileskou <bileskou@hotmail.com>
2020-11-01 00:06:04 +0800

    添加在模块中解决语雀图片不能显示问题

--- themes/icarus/source/js/main.js
+++ themes/icarus/source/js/main.js
@@ -3,6 +3,7 @@
         // wrap images with link and add caption if possible
         if ($(this).parent('a').length === 0) {
             $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            $(this).attr('referrerpolicy','no-referrer')
             if (this.alt) {
                 $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
             }
